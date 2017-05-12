74bfe013d880211d9f85712fb042c8f5491ba4ec
Mike Rousos <Mike@MJRousos.com>
2017-05-12 09:30:04 -0400

    Render game board and restrict animal movement to board

--- WebClient/src/client/scripts/components/gameBoard/gameBoard.js
+++ WebClient/src/client/scripts/components/gameBoard/gameBoard.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import React from 'react';
+import SquareTypes from '../../constants/boardSquareTypes';
+import AnimalTypes from '../../constants/animalTypes';
 import PropTypes from 'prop-types';
 
 class GameBoard extends React.Component {
@@ -8,24 +10,73 @@ class GameBoard extends React.Component {
     super(props);
   }
 
+  // Image styles
   getAnimalStyle() {
     return {
-      position: 'relative',
+      position: 'absolute',
       width: this.props.tileSize + 'px',
-      left: (this.props.gameState.animalPosition.x *  this.props.tileSize) + 'px',
-      top: (this.props.gameState.animalPosition.y * this.props.tileSize) + 'px'
+      left: (this.props.gameState.animal.position.x * this.props.tileSize) + 'px',
+      top: (this.props.gameState.animal.position.y * this.props.tileSize) + 'px'
     };
+  }
+
+  getSquareStyle(rowNumber, colNumber) {
+    return {
+      position: 'absolute',
+      width: this.props.tileSize + 'px',
+      left: (colNumber * this.props.tileSize) + 'px',
+      top: (rowNumber * this.props.tileSize) + 'px',
+      margin: '0px'
+    };
+  }
+
+  // Image sources
+  getAnimalSrc() {
+    switch (this.props.gameState.animal.type) {
+      case AnimalTypes.OCELOT:
+        return '/img/animals/Ocelot.svg';
+      default:
+        return '/img/None.svg';
+    }
+  }
+
+  getSquareSrc(squareType) {
+    switch (squareType) {
+      case SquareTypes.GRASS:
+        return '/img/terrain/Grass-1.svg';
+      default:
+        return '/img/None.svg';
+    }
+  }
+
+  // Render methods
+  renderGameBoardSquare(squareType, colNumber, row) {
+    var rowNumber = row.rowNumber;
+    return (
+      <img key={'img' + rowNumber + '-' + colNumber} style={this.getSquareStyle(rowNumber, colNumber)} src={this.getSquareSrc(squareType)} />
+    );
+  }
+
+  renderGameBoardRow(row, rowNumber) {
+    row.rowNumber = rowNumber;
+    return (
+      <div key={'row' + rowNumber}>
+        {row.map(this.renderGameBoardSquare, this)}
+      </div>
+    );
   }
 
   render() {
     return (
-      <div>
-        <img style={this.getAnimalStyle()} src="/img/animals/Ocelot.svg" />
+      <div style={{ position: 'relative' }}>
+        {this.props.gameState.board.map(this.renderGameBoardRow, this)}
+        <img style={this.getAnimalStyle()} src={this.getAnimalSrc()} />
       </div>
     );
   }
 }
 
+// Prop types
 GameBoard.propTypes = {
   gameState: PropTypes.object.isRequired,
   tileSize: PropTypes.number.isRequired
--- WebClient/src/client/scripts/stores/gameStateStore.js
+++ WebClient/src/client/scripts/stores/gameStateStore.js
@@ -1,6 +1,7 @@
 import Dispatcher from '../dispatcher/Dispatcher';
 import ActionTypes from '../constants/actionTypes';
 import InstructionTypes from '../constants/instructionTypes';
+import AnimalTypes from '../constants/animalTypes';
 import { EventEmitter } from 'events';
 
 import SquareTypes from '../constants/boardSquareTypes';
@@ -11,9 +12,9 @@ var getEmptyBoard = function (rows, columns) {
   for (var i = 0; i < rows; i++) {
     var row = List();
     for (var j = 0; j < columns; j++) {
-      row.push(SquareTypes.GRASS);
+      row = row.push(SquareTypes.GRASS);
     }
-    board.push(row);
+    board = board.push(row);
   }
 
   return board;
@@ -21,7 +22,10 @@ var getEmptyBoard = function (rows, columns) {
 
 var getInitialState = function () {
   return {
-    animalPosition: { x: 0, y: 0 },
+    animal: {
+      type: AnimalTypes.OCELOT,
+      position: { x: 0, y: 0 }
+    },
     board: getEmptyBoard(12, 12)
   };
 };
@@ -65,24 +69,24 @@ const gameStateStore = new GameStateStore();
 Dispatcher.register(function (action) {
   switch (action.actionType) {
     case ActionTypes.MOVE_ANIMAL:
-      var position = gameState.animalPosition;
+      var position = gameState.animal.position;
       switch (action.moveType) {
         case InstructionTypes.MOVE_LEFT:
-          position.x = position.x - 1;
+          position.x = Math.max(0, position.x - 1);
           break;
         case InstructionTypes.MOVE_UP:
-          position.y = position.y - 1;
+          position.y = Math.max(0, position.y - 1);
           break;
         case InstructionTypes.MOVE_DOWN:
-          position.y = position.y + 1;
+          position.y = Math.min(gameState.board.count() - 1, position.y + 1);
           break;
         case InstructionTypes.MOVE_RIGHT:
-          position.x = position.x + 1;
+          position.x = Math.min(gameState.board.first().count() - 1, position.x + 1);
           break;
         default:
           break;
       }
-      gameState.animalPosition = position;
+      gameState.animal.position = position;
       gameStateStore.emitChange();
       return gameState;
     default:
