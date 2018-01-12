95a85e15838c8dfbe8a3dfcd7717c1b861927519
Mike Rousos <Mike@MJRousos.com>
2018-01-12 12:43:40 -0500

    Update runconsole to listen for debugger and add attach debug profile

--- PuzzleService/runConsole.bat
+++ PuzzleService/runConsole.bat
@@ -1,2 +1,2 @@
 @REM Or "mvn spring-boot:run"
-java -jar %~dp0\puzzle-service-console\target\puzzle-service-console-0.0.1-SNAPSHOT.jar -q
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar %~dp0\puzzle-service-console\target\puzzle-service-console-0.0.1-SNAPSHOT.jar -q
