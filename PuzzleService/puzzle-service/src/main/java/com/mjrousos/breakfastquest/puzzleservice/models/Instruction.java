6badf09dd82bdc031f89bf1cb42dd8c18aca6c11
Mike Rousos <Mike@MJRousos.com>
2018-01-25 17:39:49 -0500

    Add more Instruction ctor overloads

--- PuzzleService/puzzle-service/src/main/java/com/mjrousos/breakfastquest/puzzleservice/models/Instruction.java
+++ PuzzleService/puzzle-service/src/main/java/com/mjrousos/breakfastquest/puzzleservice/models/Instruction.java
@@ -3,10 +3,24 @@ package com.mjrousos.breakfastquest.puzzleservice.models;
 public class Instruction {
 	private InstructionTypes type;
 	private Short target;
-	
+
+    public Instruction(InstructionTypes type) {
+        this(type, 0);
+    }
+
+    public Instruction(InstructionTypes type, int target) {
+        this(type, (short)target);
+    }
+
+    public Instruction(InstructionTypes type, short target) {
+        this.type = type;
+        this.target = target;
+    }
+
 	public InstructionTypes getType() {
 		return type;
-	}
+    }
+
 	public void setType(InstructionTypes type) {
 		this.type = type;
 	}
@@ -16,5 +30,5 @@ public class Instruction {
 	public void setTarget(Short target) {
 		this.target = target;
 	}
-	
+
 }
