b910d51fad46e75db8b51f2dc0bf84b4512e42fd
Mike Rousos <Mike@MJRousos.com>
2018-01-28 23:04:01 -0500

    Allow solution requirements min/max inventory arrays to be set

--- PuzzleService/puzzle-service/src/main/java/com/mjrousos/breakfastquest/puzzleservice/models/SolutionRequirements.java
+++ PuzzleService/puzzle-service/src/main/java/com/mjrousos/breakfastquest/puzzleservice/models/SolutionRequirements.java
@@ -5,37 +5,45 @@ public class SolutionRequirements {
 	private int[] minInventoryCounts;
 	private int[] maxInventoryCounts;
 	private short[][] boardState;
-	
+
 	public SolutionRequirements() {
 		// Default to only expecting breakfast to be picked up
 		int[] expectedInventory = new int[Items.values().length];
 		expectedInventory[Items.Breakfast.ordinal()] = 1;
-		
+
 		minInventoryCounts = expectedInventory.clone();
 		maxInventoryCounts = expectedInventory.clone();
-		
+
 	}
-	
+
 	public short[][] getBoardState() {
 		return boardState;
 	}
 	public void setBoardState(short[][] boardState) {
 		this.boardState = boardState;
 	}
-	
+
 	public int getMinInventoryCount(int i) {
 		return minInventoryCounts[i];
 	}
-	
+
 	public int getMaxInventoryCount(int i) {
 		return maxInventoryCounts[i];
 	}
-	
+
 	public int[] getMinInventoryCounts() {
 		return minInventoryCounts;
-	}
+    }
+
 	public int[] getMaxInventoryCounts() {
 		return maxInventoryCounts;
 	}
-		
+
+    public void setMinInventoryCounts(int[] counts) {
+        this.minInventoryCounts = counts;
+    }
+
+    public void setMaxInventoryCounts(int[] counts) {
+        this.maxInventoryCounts = counts;
+    }
 }
